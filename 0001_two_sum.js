5a071e403ee208022b33faba5bf245d5d33e70d5
SergioBonatto <bonatto@tutanota.com>
2026-06-13 13:29:06 -0300

    add: 0001_two_sum.js

---
+++ 0001_two_sum.js
@@ -0,0 +1,34 @@
+/*
+LeetCode #1
+Two Sum
+
+Return indices of the two numbers such that they add up to target.
+
+Difficulty: Easy
+Day: 001
+
+https://leetcode.com/problems/two-sum/
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6));      // [1, 2]
+console.log(twoSum([3, 3], 6));         // [0, 1]
